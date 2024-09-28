69297990066271ad32ba16a95856f49e9a3f4cc8
Rahul Mishra <rahulmishra21791@gmail.com>
2024-09-28 09:58:31 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MemoryGame from "./memory-game/memory-game";
+
+function App() {
+  return (
+    <div className="app">
+      <MemoryGame />
+    </div>
+  );
+}
+
+export default App;
